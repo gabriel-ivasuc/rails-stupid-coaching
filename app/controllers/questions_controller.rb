a9dafc53ff82533c04377bf713e207ccca3b1754
Gabriel Ivasuc <gabrielivasuc@yahoo.com>
2024-04-29 18:44:49 +0100

    added style to application.css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,16 +5,14 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    @answer = generate_coach_answer(@question)
-  end
-
-  def generate_coach_answer(question)
-    if question == 'i am going to work'
-      'Great!'
-    elsif question.end_with?('?')
-      'Silly question, get dressed and go to work!'
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = 'Great!'
+    elsif @question.ends_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
     else
-      "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
